f79e29f9b9c646e4e8923a121dcc7d032c1ab9bd
shivamkoli <67708684+shivam7053@users.noreply.github.com>
2025-12-22 10:03:23 +0000

    frontend urls added

--- main.py
+++ main.py
@@ -6,20 +6,17 @@ from image_tools import router as img_router
 
 app = FastAPI()
 
-# Allow your React app to talk to this backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # In production, replace with your Netlify URL
+    allow_origins=[
+        "https://duniyadari.netlify.app",
+        "https://dailytoolsss.netlify.app"
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Include Routers
 app.include_router(pdf_router)
 app.include_router(doc_router)
 app.include_router(img_router)
-
-if __name__ == "__main__":
-    import uvicorn
-    # reload=True allows the server to update automatically when you save code
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
